5ec7fa7834c544b565143293a0f0d923c0017d29
Leerie Simpson <leerie.a.simpson@gmail.com>
2020-11-05 08:11:30 -0500

    Updated min requirements.

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.3
+// swift-tools-version:5.3.0
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "BroadcastKit",
     platforms: [
-        .macOS("10.15"), .iOS("13.0"),
+        .macOS("11.0"), .iOS("14.0"),
     ],
 
     products: [
